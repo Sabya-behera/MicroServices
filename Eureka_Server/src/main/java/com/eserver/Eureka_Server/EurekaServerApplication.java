ec083c0d59a6b405f4008d47cade91a2fad1d91d
sabya <sabyasacheebeherappl@gmail.com>
2021-10-29 12:41:40 +0530

    new User

--- Eureka_Server/src/main/java/com/eserver/Eureka_Server/EurekaServerApplication.java
+++ Eureka_Server/src/main/java/com/eserver/Eureka_Server/EurekaServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer   //This is a server class
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
--- Microservices-User_Service/src/main/java/com/example/Microservices/Repository/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.Microservices.Repository;
-
-
-import com.example.Microservices.Model.User;
-
-public interface UserRepo  {
-    public User getUser(Long id);
-}
